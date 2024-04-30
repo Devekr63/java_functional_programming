8f76d8574d53f01626d22576793aa1ac1b25de9f
Devendra <devekr12@gmail.com>
2024-04-30 18:39:26 +0530

    anonumous class

---
+++ src/main/java/org/example/nestedclasses/Main.java
@@ -0,0 +1,29 @@
+package org.example.nestedclasses;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    static class SomeClass{
+        private String name= "Devendra.";
+    }
+
+    class InnerClass{
+        String name = "Kumar";
+    }
+    public static void main(String[] args) {
+        SomeClass sc = new SomeClass();
+        System.out.println(sc.name);
+        Main main = new Main();
+        main.doSomeThing();
+    }
+
+    private void doSomeThing(){
+        SomeClass sc = new SomeClass();
+        InnerClass ic = new InnerClass();
+        System.out.println(sc.name+" "+ic.name);
+    }
+}
+
+
+
